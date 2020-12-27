098a6b23d6a286adbaea63c79b851b19fe5ac33d
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2020-12-27 17:37:49 +0100

    first before heroku server setup

---
+++ client/script.js
@@ -0,0 +1,29 @@
+const sendBtn = document.querySelector('#send-button');
+var IDinput = document.querySelector('#id-input');
+var formatinput = document.querySelector('#format-input');
+
+sendBtn.addEventListener('click',()=>{
+        window.location.href = `http://localhost:3000/download?v_id=${IDinput.value}&format=${formatinput.value}`;
+});
+
+function copyExample(element){
+    var range, selection, worked;
+
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+    } else if (window.getSelection) {
+      selection = window.getSelection();        
+      range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+    
+    try {
+      document.execCommand('copy');
+    }
+    catch (err) {
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const youtube_dl = require('youtube-dl');
+
+const app = express();
+app.use(cors());
+
+app.get('/download',async (req,res)=>{
+    try{
+        var v_id = req.query.v_id;
+        var formtCode = req.query.format;
+        var URL = `https://youtu.be/${v_id}`;
+       
+        let info = await ytdl.getInfo(v_id);
+        console.log(info.videoDetails)
+        res.header('Content-Disposition', 'attachment; filename="'+info.videoDetails.title+'.mp4"');
+        let format = ytdl.chooseFormat(info.formats, { quality: formtCode });
+
+        ytdl(URL,{format}).pipe(res);
+    }catch (err){
+        console.log(err)
+        res.status(500).send();
+    }
+
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+});
