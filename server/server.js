be7d8812d235ccbb3ec4338970452d329f26be0f
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2020-12-28 12:05:28 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,14 +8,22 @@ const app = express();
 const PORT = process.env.PORT||3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.set("Access-Control-Expose-Headers", 'Content-Length');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//     res.set("Access-Control-Expose-Headers", 'Content-Length');
+//     next();
+// });
 
 
 app.get('/download',async (req,res)=>{
