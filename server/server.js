e12ad94be2d99ed4f3aa8f8cb3a53129308e5e9e
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2020-12-28 12:01:57 +0100

    update origin allow

--- server/server.js
+++ server/server.js
@@ -10,9 +10,11 @@ const PORT = process.env.PORT||3000;
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.set("Access-Control-Expose-Headers", 'Content-Length');
-	next();
+    next();
 });
 
 
