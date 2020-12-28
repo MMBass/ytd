f04dedf68818bfc26e3d4067573eabbc200d3af4
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2020-12-28 12:23:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT||3000;
 
 app.use(cors());
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
