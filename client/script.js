998dd9678b9b2b420f4836dead64aa10470d08f9
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2020-12-28 12:08:30 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -3,8 +3,7 @@ var IDinput = document.querySelector('#id-input');
 var formatinput = document.querySelector('#format-input');
 
 sendBtn.addEventListener('click',()=>{
-//   fetch(`https://bass-ytd.herokuapp.com?v_id=${IDinput.value}&format=${formatinput.value}`);
-  fetch(`https://bass-ytd.herokuapp.com`);
+  fetch(`https://bass-ytd.herokuapp.com/download?v_id=${IDinput.value}&format=${formatinput.value}`);
         // window.location.href = 
 });
 
