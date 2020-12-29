b95d9e38ba3bb5baad4738eab92fd1de79495ea0
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2020-12-29 14:07:51 +0100

    Add files via upload

--- client/script.js
+++ client/script.js
@@ -1,11 +1,12 @@
 const sendBtn = document.querySelector('#send-button');
 const getInfoBtn = document.querySelector('#get-info-button');
+let main = document.querySelectorAll('main')[0];
 var IDinput = document.querySelector('#id-input');
 var formatInput = document.querySelector('#format-input');
 var selectSection = document.querySelector('#select-section');
 var firstH6 = document.querySelector('#first-h6');
-const base_url = "https://bass-ytd.herokuapp.com/";
-// const base_url = "http://localhost:5000/";
+// const base_url = "https://bass-ytd.herokuapp.com/";
+const base_url = "http://localhost:5000/";
 
 
 getInfoBtn.addEventListener('click',()=>{
@@ -21,6 +22,15 @@ getInfoBtn.addEventListener('click',()=>{
 sendBtn.addEventListener('click',()=>{
   sendBtn.className = "btn btn-danger progress-bar bg-danger progress-bar-striped progress-bar-animated mb-2";
   window.location.href = `${base_url}download?v_id=${IDinput.value}&format=${formatInput.value}`;
+  
+  setTimeout(()=>{
+  getInfoBtn.style.display = "block";
+  getInfoBtn.className = "btn btn-warning mb-2";
+  IDinput.style.display = "block";
+  firstH6.style.display = "block";
+  selectSection.style.display = "none";
+  sendBtn.style.display = "none";
+  },1000);
 });
 
 function creaetOptions(list){
