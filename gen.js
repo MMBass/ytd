5bf3c0c63e422345b9d9299c1807e65bc3bfbcff
unknown <mendibass@gmail.com>
2021-11-21 18:22:45 +0200

    cli gen files config for components

---
+++ gen.js
@@ -0,0 +1,19 @@
+const { generateTemplateFiles } = require('generate-template-files');
+
+// const config = require('./package.json');
+
+generateTemplateFiles([
+  {
+    option: 'c',
+    defaultCase: '(pascalCase)',
+    entry: {
+      folderPath: './src/templates/component' ,
+    },
+    stringReplacers: ['name', { question: 'Insert model name', slot: 'name' }],
+    output: {
+      path: './src/components/name',
+      pathAndFileNameDefaultCase: '(pascalCase)',
+      overwrite: true,
+    },
+  },
+]);
---
+++ src/templates/component/name.js
@@ -0,0 +1,11 @@
+import "./comp_name.scss";
+
+function comp_name() {
+  return (
+    <div id="">
+      
+    </div>
+  );
+}
+
+export default comp_name;
