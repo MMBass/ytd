90433b48d9ef640872595226f3e5d7a38b2c648a
unknown <mendibass@gmail.com>
2022-01-02 10:54:27 +0200

    setings item margin + flex

--- src/pages/SettingsPage/SettingsPage.js
+++ src/pages/SettingsPage/SettingsPage.js
@@ -12,12 +12,12 @@ function SettingsPage() {
 
     function changeGlobalFormat(e) {
         let newFormat = e.target.value;
-        
-        if(newFormat === 'choose'){
+
+        if (newFormat === 'choose') {
             window.localStorage.removeItem('globalFormat');
         }
-        else{
-            window.localStorage.setItem('globalFormat',newFormat);
+        else {
+            window.localStorage.setItem('globalFormat', newFormat);
         }
         window.location.reload();
     }
@@ -33,42 +33,50 @@ function SettingsPage() {
             <NavLink to="../yt-downloader-site/" className="back-arrow"><MdOutlineArrowBack></MdOutlineArrowBack></NavLink>
             <InnerFrame id="SETTINGS_PAGE">
                 <div className="settings-content">
-                    <div className="settings-item"  onChange={changeGlobalFormat}>
+                    <div className="settings-item" onChange={changeGlobalFormat}>
                         <p>Global Quality</p>
-                        <div>
-                            <input type="radio" name="globalFormat" value='audio' disabled={mode === 'music'} checked={globalFormat === 'audio'}/>
+                        <div className="settings-line">
+                            <input type="radio" name="globalFormat" value='audio' disabled={mode === 'music'} checked={globalFormat === 'audio'} />
                             <label htmlFor="audio">Audio - mp3</label>
                         </div>
-                        <div>
-                            <input type="radio" name="globalFormat" value='136' disabled={mode === 'music'} checked={globalFormat === '136'}/>
+                        <div className="settings-line">
+                            <input type="radio" name="globalFormat" value='136' disabled={mode === 'music'} checked={globalFormat === '136'} />
                             <label htmlFor="720">720p (If exist)</label>
                         </div>
-                        <div>
-                            <input type="radio" name="globalFormat" value='18' disabled={mode === 'music'} checked={globalFormat === '18'}/>
+                        <div className="settings-line">
+                            <input type="radio" name="globalFormat" value='18' disabled={mode === 'music'} checked={globalFormat === '18'} />
                             <label htmlFor="720">360p (If exist)</label>
                         </div>
-                        <div>
-                            <input type="radio" name="globalFormat" value='lowestvideo' disabled={mode === 'music'} checked={globalFormat === 'lowestvideo'}/>
+                        <div className="settings-line">
+                            <input type="radio" name="globalFormat" value='lowestvideo' disabled={mode === 'music'} checked={globalFormat === 'lowestvideo'} />
                             <label htmlFor="lowest">Lowest (video)</label>
                         </div>
-                        <div>
-                            <input type="radio" name="globalFormat" value='highestvideo' disabled={mode === 'music'} checked={globalFormat === 'highestvideo'}/>
+                        <div className="settings-line">
+                            <input type="radio" name="globalFormat" value='highestvideo' disabled={mode === 'music'} checked={globalFormat === 'highestvideo'} />
                             <label htmlFor="highest">Highest (video)</label>
                         </div>
-                        <div>
-                            <input type="radio" name="globalFormat" value='choose' disabled={mode === 'music'}checked={globalFormat == null}/>
+                        <div className="settings-line">
+                            <input type="radio" name="globalFormat" value='choose' disabled={mode === 'music'} checked={globalFormat == null} />
                             <label htmlFor="choose">Choose-every-time</label>
                         </div>
                     </div>
 
-                    <div className="settings-item" onChange={changeMode}>
+                    <div className="settings-item mode-settings" onChange={changeMode}>
                         <p>Mode</p>
-                        <input type="radio" name="mode" value='music' checked={mode === 'music'}/>
-                        <label htmlFor="music">music</label>
-                        <input type="radio" name="mode" value='playlist'  checked={mode === 'playlist'}/>
-                        <label htmlFor="playlist">playlist</label>
-                        <input type="radio" name="mode" value='video'  checked={mode === 'video'}/>
-                        <label htmlFor="video">video</label>
+                        <div className="settings-line">
+                            <input type="radio" name="mode" value='music' checked={mode === 'music'} />
+                            <label htmlFor="music">music</label>
+                        </div>
+                        <div className="settings-line">
+                            <input type="radio" name="mode" value='playlist' checked={mode === 'playlist'} />
+                            <label htmlFor="playlist">playlist</label>
+                        </div>
+                        <div className="settings-line">
+                            <input type="radio" name="mode" value='video' checked={mode === 'video'} />
+                            <label htmlFor="video">video</label>
+                        </div>
+
+
                     </div>
 
                 </div>
